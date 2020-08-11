eebda84689efdeda7e28eb045f728e07f8531073
AmrAkmal <amrakmal97@gmail.com>
2020-08-11 21:43:22 +0200

    take input

--- graderx/graders/preset_graders/c/c_grader.py
+++ graderx/graders/preset_graders/c/c_grader.py
@@ -1,11 +1,20 @@
 import sys
 import os
+import shlex
+import subprocess
 from pathlib import Path
 import lib.compile_submission as compiler
 
 COURSE_ABS_PATH = None
 LAB_ABS_PATH = None
 
+test_cases = [
+    ("1", "Hello\nWorld\n", "HelloWorld\n"),
+    ("2", "Hakuna\nMatata\n", "HakunaMatata\n"),
+    ("3", "First\nSecond\n", "FirstSecond\n"),
+    ("4", "Fizz\nBuzz\n", "FizzBuzz\n"),
+    ("5", "Sneaky \nTestCase\n", "Sneaky TestCase\n")
+]
 
 def main():
     """
@@ -21,6 +30,10 @@ def main():
         for i in dir:
             s = str(LAB_ABS_PATH.joinpath(f"submissions")) + f"/{i}"
             compiler.compile_submission(Path(s))
+            s = "./a.out"
+            cmd = shlex.split(["./a.out", ])
+            cprocess = subprocess.run(
+            cmd, cwd=Path(s), capture_output=True, text=True)
     pass
 
 
