f69da1865ac0df61d5162b1f727eec811ddb7024
AmrAkmal <amrakmal97@gmail.com>
2020-08-11 12:27:11 +0200

    Get count of current submissions and current test cases to be used and graded.

--- graderx/graders/preset_graders/c/c_grader.py
+++ graderx/graders/preset_graders/c/c_grader.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from pathlib import Path
 import lib.compile_submission as compiler
 
@@ -15,6 +16,17 @@ def main():
     except compiler.CompilationFailedError as e:
         print(e)
     """
+    test_cases_count = submissions_count = 0
+    for _, dirnames, filenames in os.walk(LAB_ABS_PATH.joinpath(f"submissions")):
+        submissions_count += len(dirnames)
+        break
+    
+    for _, dirnames, filenames in os.walk(LAB_ABS_PATH.joinpath(f"test_cases")):
+        test_cases_count += len(filenames)
+        break
+
+    test_cases_count = int(test_cases_count / 2)
+    print(submissions_count)
     pass
 
 
