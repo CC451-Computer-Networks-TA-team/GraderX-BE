59cbf7147338ec9308160b410b70b57ca8f3f6a9
amr <amrfahmy57@gmail.com>
2020-08-12 23:31:07 +0200

    integrate with moss

--- graderx/graders/manager.py
+++ graderx/graders/manager.py
@@ -1,15 +1,15 @@
-import shutil
-import glob
-import patoolib
-import time
 from werkzeug.datastructures import FileStorage
 from pathlib import Path
 import shlex
 import subprocess
 import os
-# dependencies for docker
+import mosspy
 
-currentStatusString = ""
+# dependencies for docker
+import patoolib
+import glob
+import shutil
+# pip install patool
 
 
 def clean_directory(dir):
@@ -32,13 +32,10 @@ def extract_submissions(dest_directory, submissions_file,  verbosity=0):
     Args:
     dest_directory: Path. Submission directory found in [lab_name]/config.py
     submissions_file: FileStorage. It is used by the request object to represent uploaded files. 
-
     Returns: bool
     status: True on sucessful extraction
-
     Actions:
     extracts the submissions file in the destenation directory and removes the rar (or Whatever) file
-
     """
 
     file_name = submissions_file.filename
@@ -77,20 +74,30 @@ def run_grader_commands(lab_id):
                 f"python {curr_dir}/courses/cc451/app/lib/console_log_parser.py {lab_number} {curr_dir}/courses/cc451/app/res/{lab_id}")
             subprocess.run(cmd, stdin=fi, stdout=fo)
 
+def moss(lab_id: str):
+    userid = 631291500
+    m = mosspy.Moss(userid, "python")
+    # Add base files if any at '/courses/cc451/app/{lab_id}/base_files/2020/*.py'
+    dir = '{curr_dir}/courses/cc451/app/{lab_id}/base_files/2020'
+    try:
+        for filename in os.listdir(dir):
+            if filename.endswith(".py"):
+                m.addBaseFile(filename)
+    except:
+        print("No base files")
+    m.addFilesByWildcard("{curr_dir}/courses/cc451/app/{lab_id}/submissions/2020/*.py")
+    url = m.send() 
 
-def get_status():
-    return currentStatusString
-
+    file_name = "{curr_dir}/courses/cc451/app/res/moss_output.txt"
+    with open(file_name, "w+") as f:
+        f.write(url)
 
 def run_grader(lab_id: str, submissions_file: FileStorage) -> dict:
-    global currentStatusString
     curr_dir = str(Path(__file__).parent.resolve())
-    currentStatusString = "Processing"
     extract_submissions(Path(
         f"{curr_dir}/courses/cc451/app/{lab_id}/submissions/2020"), submissions_file)
-    currentStatusString = "Grading"
     run_grader_commands(lab_id)
-    currentStatusString = ""
+    moss(lab_id)
 
 
 def save_single_submission(lab_id, submission_file, file_name):
