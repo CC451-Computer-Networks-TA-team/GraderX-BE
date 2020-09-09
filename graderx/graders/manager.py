6bcd34e6e88ec1af07e0650e3e17b988a9390b80
amroakmal <amrakmal97@gmail.com>
2020-09-09 23:43:51 +0200

    edit manager for checking new courses name

--- graderx/graders/manager.py
+++ graderx/graders/manager.py
@@ -25,9 +25,9 @@ def add_new_course_to_current_courses(data):
 
 def create_course(course_name, language, labs):
     current_courses = get_courses() 
-    # for course in current_courses:
-    #     if course.lower() == course_name.lower():
-    #         return "Course already present", 500
+    for course in current_courses:
+        if course.lower() == course_name.lower():
+            return "Course already present", 500
     current_courses = get_courses_config()
     current_courses[course_name] = {"course_type": "stdout", "language": language, "labs": labs}
     add_new_course_to_current_courses(current_courses)
