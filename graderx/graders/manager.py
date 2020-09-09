1ce34edceb6a3c938ba0362d14b7dd95645aaf4a
AmrAkmal <amrakmal97@gmail.com>
2020-09-09 23:25:48 +0200

    createnew directory for labs and test cases

--- graderx/graders/manager.py
+++ graderx/graders/manager.py
@@ -25,13 +25,13 @@ def add_new_course_to_current_courses(data):
 
 def create_course(course_name, language, labs):
     current_courses = get_courses() 
-    for course in current_courses:
-        if course.lower() == course_name.lower():
-            return "Course already present", 500
+    # for course in current_courses:
+    #     if course.lower() == course_name.lower():
+    #         return "Course already present", 500
     current_courses = get_courses_config()
     current_courses[course_name] = {"course_type": "stdout", "language": language, "labs": labs}
     add_new_course_to_current_courses(current_courses)
-    # stdout_common.create_course(course_name, language, labs, get_courses())
+    stdout_common.create_course_data(course_name, labs)
 
 
 def get_courses():
--- graderx/graders/stdout_graders/stdout_common/stdout_common.py
+++ graderx/graders/stdout_graders/stdout_common/stdout_common.py
@@ -2,3 +2,28 @@ from pathlib import Path
 import json
 
 
+def create_course_data(course_name, labs):
+    path = Path(__file__).parent.parent.parent.joinpath("courses") / course_name
+    path.mkdir()
+    path = path / "labs"
+    path.mkdir()
+    dir_path = path
+    for lab in labs:
+        path = path / lab['name']
+        path.mkdir()
+        path = path / 'test_cases'
+        path.mkdir()
+        pre_tc = path
+        for tc in lab['test_cases']:
+            path = Path(path, f"{tc['tc_id']}_in")
+            path.touch()
+            with path.open('w') as wf:
+                wf.write(tc['input'])
+            path = pre_tc
+            path = Path(path, f"{tc['tc_id']}_out")
+            path.touch()
+            with path.open('w') as wf:
+                wf.write(tc['output'])
+            path = pre_tc
+        path = dir_path
+
