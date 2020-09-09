a45a1d653fbe6d6c8c225e3b4d4f001063b07c1e
AmrAkmal <amrakmal97@gmail.com>
2020-09-09 22:02:52 +0200

    Add new course to courses_confing

--- graderx/graders/manager.py
+++ graderx/graders/manager.py
@@ -17,8 +17,22 @@ def get_courses_config():
     with open(Path(__file__).parent.joinpath("courses_config.json")) as f:
         return json.load(f)
 
-def create_course(course_name, language, labs):    
-    stdout_common.create_course(course_name, language, labs, get_courses())
+
+def add_new_course_to_current_courses(data):
+    with open(Path(__file__).parent.joinpath("courses_config.json"), 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+
+
+def create_course(course_name, language, labs):
+    current_courses = get_courses() 
+    for course in current_courses:
+        if course.lower() == course_name.lower():
+            return "Course already present", 500
+    current_courses = get_courses_config()
+    current_courses[course_name] = {"course_type": "stdout", "language": language, "labs": labs}
+    add_new_course_to_current_courses(current_courses)
+    # stdout_common.create_course(course_name, language, labs, get_courses())
+
 
 def get_courses():
     return list(get_courses_config().keys())
--- graderx/graders/stdout_graders/stdout_common/stdout_common.py
+++ graderx/graders/stdout_graders/stdout_common/stdout_common.py
@@ -2,12 +2,3 @@ from pathlib import Path
 import json
 
 
-def create_course(course_name, language, labs, current_courses):
-    for course in current_courses:
-        if course.lower() == course_name.lower():  
-            return "Course already present", 500    
-
-def edit_courses_config(data):
-    courses_config_path = Path(__file__).parent.parent.parent.joinpath("courses_config.json")
-    with open(courses_config_path, 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=2)
