401101fa6de88a475a40d780976e9269f9c30ac9
AmrAkmal <amrakmal97@gmail.com>
2020-09-09 23:32:37 +0200

    code refactoring

--- graderx/graders/stdout_graders/stdout_common/stdout_common.py
+++ graderx/graders/stdout_graders/stdout_common/stdout_common.py
@@ -1,6 +1,11 @@
 from pathlib import Path
 import json
 
+def add_test_case_file(path, tc_name, data):
+    path = Path(path, tc_name)
+    path.touch()
+    with path.open('w') as wf:
+        wf.write(data)
 
 def create_course_data(course_name, labs):
     path = Path(__file__).parent.parent.parent.joinpath("courses") / course_name
@@ -13,17 +18,19 @@ def create_course_data(course_name, labs):
         path.mkdir()
         path = path / 'test_cases'
         path.mkdir()
-        pre_tc = path
+        # pre_tc = path
         for tc in lab['test_cases']:
-            path = Path(path, f"{tc['tc_id']}_in")
-            path.touch()
-            with path.open('w') as wf:
-                wf.write(tc['input'])
-            path = pre_tc
-            path = Path(path, f"{tc['tc_id']}_out")
-            path.touch()
-            with path.open('w') as wf:
-                wf.write(tc['output'])
-            path = pre_tc
+            add_test_case_file(path, f"{tc['tc_id']}_in", tc['input'])
+            add_test_case_file(path, f"{tc['tc_id']}_out", tc['output'])
+            # path = Path(path, f"{tc['tc_id']}_in")
+            # path.touch()
+            # with path.open('w') as wf:
+            #     wf.write(tc['input'])
+            # path = pre_tc
+            # path = Path(path, f"{tc['tc_id']}_out")
+            # path.touch()
+            # with path.open('w') as wf:
+            #     wf.write(tc['output'])
+            # path = pre_tc
         path = dir_path
 
