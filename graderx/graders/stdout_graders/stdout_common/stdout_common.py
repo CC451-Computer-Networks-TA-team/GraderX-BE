54c97762e8bf1508416cf178f73d7ccdc8df0891
AmrAkmal <amrakmal97@gmail.com>
2020-09-09 20:26:56 +0200

    Add course_creation method and handling repeated ones

--- graderx/graders/stdout_graders/stdout_common/stdout_common.py
+++ graderx/graders/stdout_graders/stdout_common/stdout_common.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 import json
 
+
 def create_course(course_name, language, labs, current_courses):
-    for i in current_courses:
-        if i.lower() == course_name.lower():  
-            return "Course already present", 500
-    
+    for course in current_courses:
+        if course.lower() == course_name.lower():  
+            return "Course already present", 500    
 
 def edit_courses_config(data):
     path = Path(__file__).parent.parent.parent.joinpath("courses_config.json")
