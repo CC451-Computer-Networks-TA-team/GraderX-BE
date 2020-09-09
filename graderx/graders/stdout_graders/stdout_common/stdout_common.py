b8cb340b8c28e5670ad5c1c59148dc2fa925888e
AmrAkmal <amrakmal97@gmail.com>
2020-09-09 20:27:35 +0200

    add course_config method for editing/writing to it

--- graderx/graders/stdout_graders/stdout_common/stdout_common.py
+++ graderx/graders/stdout_graders/stdout_common/stdout_common.py
@@ -8,6 +8,6 @@ def create_course(course_name, language, labs, current_courses):
             return "Course already present", 500    
 
 def edit_courses_config(data):
-    path = Path(__file__).parent.parent.parent.joinpath("courses_config.json")
-    with open(path, 'w', encoding='utf-8') as f:
+    courses_config_path = Path(__file__).parent.parent.parent.joinpath("courses_config.json")
+    with open(courses_config_path, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
